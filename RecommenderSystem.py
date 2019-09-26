4210b57a88e237c8ae1f50c03f348e470888c821
Your Name Brian <bshuryan@oakland.edu>
2019-09-26 16:21:00 -0400

    Work on recommender system- add imdb database files for further testing

--- RecommenderSystem.py
+++ RecommenderSystem.py
@@ -5,8 +5,12 @@ import sklearn
 import math
 
 
-
-
-
+def main():
+    imdb_titles = pd.read_csv('title.basics.tsv', sep='\t')
+    print(imdb_titles.head())
+    print(imdb_titles.tail())
+    
+if __name__ == '__main__':
+    main()
 
 
